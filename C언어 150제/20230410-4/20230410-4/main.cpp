344266fe3619d2209077128b2fa53742ffb4cfe7
kjwqqqq <125715176+kjwqqqq@users.noreply.github.com>
2023-04-10 21:46:43 +0900

    Add files via upload

---
+++ C언어 150제/20230410-4/20230410-4/main.cpp
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+void main()
+{
+	int x, y;
+	x = 0;
+	y = 0;
+	scanf("%d", &x);
+	scanf("%d", &y);
+	if (x > y)
+	{
+		printf("x의 값이 y의 값보다 큽니다. \n");
+	}
+	else (x < y);
+	{
+		printf("x의 값이 y의 값보다 작습니다.");
+	}
+
+}
