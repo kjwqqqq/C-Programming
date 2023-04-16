e8d505a7585dd0f4160688560a195a4e678a6806
kjwqqqq <125715176+kjwqqqq@users.noreply.github.com>
2023-04-16 23:01:27 +0900

    Add files via upload

---
+++ C언어 150제/20230414/20230414-14/20230414-14/main.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+void main()
+{
+	bool b;
+	b = 10 > 5;
+
+	printf("b = %d\n", b); // 참인 경우는 1 거짓 경우 0
+}
---
+++ C언어 150제/20230414/20230414-15/20230414-15/main.cpp
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void main()
+{
+	char str[] = "대한민국";
+
+
+	printf("%s은 우리나라입니다", str);
+}
