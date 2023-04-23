3599331cb6afddf7589da1d39bf75ec0ff5b2b86
kjwqqqq <125715176+kjwqqqq@users.noreply.github.com>
2023-04-23 22:12:49 +0900

    Add files via upload

---
+++ 0423/20230423-15/main.cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void main()
+{
+    int n;
+
+    printf("************************\n");
+    for (int i = 0; i < 5; i++)
+        printf("*                      *\n");
+    printf("************************\n");
+
+}
---
+++ 0423/20230423-17/main.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void main()
+{
+    int x, y;
+
+    for (y = 0; y <= 5; y++)
+    {
+        for (x = 0; x < 10; x++)
+            printf("*");
+
+        printf("\n");
+    }
+
+
+}
